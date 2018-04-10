60f0a7080e797e05816125211371f036bf105ae7
Marcos Vinícius Adão Teixeira <ecclesiedei@gmail.com>
2018-04-10 19:53:57 -0300

    fixing bugs

--- iftFastRadonTransform2D.c
+++ iftFastRadonTransform2D.c
@@ -1,17 +1,15 @@
 #include "ift.h"
 
-
 #define ROUND(x) ((x < 0)?(int)(x-0.5):(int)(x+0.5))
-#define GetVoxelIndex(s,v) ((v.x)+(s)->tby[(v.y)])
+//#define GetVoxelIndex(s,v) ((v.x)+(s)->tby[(v.y)])
 
 int sign( int x ){
     if(x > 0)
-            return 1;
+        return 1;
     if(x < 0) 
-            return -1;
+        return -1;
     return 0;
 }
-
 
 
 /* this function creas the rotation/translation matrix for the given theta */
@@ -33,12 +31,11 @@ iftMatrix *createRadonMatrix(iftImage *img, int theta)
     return resMatrix;
 }
 
-/* this function uses de DDA algorithm to paint a line from p1 to pn in img */
-iftFloatList DDA(iftImage *img, iftVoxel p1, iftVoxel pn)
+float* DDA(iftImage *img, iftVoxel p1, iftVoxel pn)
 {
     int n, k;
     iftVoxel p;
-    iftFloatList * J;
+    float * J;
     float Dx=(pn.x - p1.x),Dy=(pn.y - p1.y);
     float dx=0,dy=0;
     //iftCreateMatrix* J;
@@ -62,32 +59,84 @@ iftFloatList DDA(iftImage *img, iftVoxel p1, iftVoxel pn)
         }
     }
 
-    J = CreateFloatList(line->n);
+    J = (float*)(malloc(n*sizeof(float)));
     p=p1;
 
     // TODO: calcular I como interpolacao
 
+    
     for (k = 0; k < n; k++)
     {
-        J->val[k] img->val[ROUND(p.x)][ROUND(p.y)];
+        //J[k] = img->val[ROUND(p.x)][ROUND(p.y)];
+        J[k] =  iftImgVal2D(img, p.x, p.y);
 
 
         p.x = p.x + dx;
         p.y = p.y + dy;
     }
+
     return J;
 }
 
-int isValidPoint(Image *img, Voxel u)
+
+int isValidPoint(iftImage *img, iftVoxel u)
 {
     if ((u.x >= 0) && (u.x < img->xsize) &&
-            (u.y >= 0) && (u.y < img->ysize))
+        (u.y >= 0) && (u.y < img->ysize)){
         return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+
+int findIntersection(iftMatrix *Po, iftImage *img, iftMatrix *N,int nx, int ny, iftVoxel *p1, iftVoxel *pn){
+    float lamb1,lamb2,lamb3,lamb4;    
+    int i;
+    float max=0.0,min=999999999.0;
+    iftVoxel v1, v2;
+    p1->x=pn->x=p1->y=pn->y=-1;
+
+
+    lamb1 = -(Po->val[1])/iftMax(N->val[1],1);
+    lamb2 = (ny - Po->val[1])/iftMax(N->val[1],1);
+    lamb3 = -(Po->val[1])/iftMax(N->val[0],1);
+    lamb4 = (nx - Po->val[0])/iftMax(N->val[0],1);
+    float lambs[] = {lamb1,lamb2,lamb3,lamb4};
+
+    for(i=0; i < 4; i++){
+        if (lambs[i]>max)
+            max = lambs[i];
+        if (lambs[i]<min)
+            min = lambs[i];
+    }
+
+    v1.x = ROUND(Po->val[0] + min * N->val[0]);
+    v1.y = ROUND(Po->val[1] + min * N->val[1]);
+
+    if (isValidPoint(img, v1)){
+        *p1=v1;
+    }
+
+    v2.x = ROUND(Po->val[0] + max * N->val[0]);
+    v2.y = ROUND(Po->val[1] + max * N->val[1]);
+
+    if (isValidPoint(img, v2)){
+        *pn=v2;
+        printf("%d, %d, %d, %d\n",p1->x,p1->y,pn->x,pn->y );
+    }
+
+    
+    if ((p1->x != -1) && (pn->x != -1) && (p1->y != -1) && (pn->y != -1)){
+        return 1;
+    }
     else
         return 0;
 }
 
-void Intersection(iftMatrix *Po, iftImage *img, iftMatrix *N,int nx, int ny, iftVoxel *p1, iftVoxel *pn)
+
+int Intersection(iftMatrix *Po, iftImage *img, iftMatrix *N,int nx, int ny, iftVoxel *p1, iftVoxel *pn)
 {
     // check interscetion to image 
     // 4 cases
@@ -100,45 +149,59 @@ void Intersection(iftMatrix *Po, iftImage *img, iftMatrix *N,int nx, int ny, ift
     // terceiro caso: lamb= -x0/Nx
     // quarto caso: lamb= (nx-1 - x0)/Nx
 
-    float lamb ;
+    float lamb;
+    int found=0;
     iftVoxel v;
 
     // first
     // lamb= -y0/Ny
-    lamb = -(Po->val[1])/N->val[1];
+
+    lamb = -(Po->val[1])/iftMax(N->val[1],1);
     v.x = ROUND(Po->val[0] + lamb * N->val[0]);
     v.y = ROUND(Po->val[1] + lamb * N->val[1]);
-    if (ValidVoxel(img, v))
+    if (isValidPoint(img, v)){
         *p1 = v;
+    }
 
     // sec
     // lamb= (ny-1 - y0)/Ny
-    lamb = (ny - Po->val[1])/N->val[1];
+    lamb = (ny - Po->val[1])/iftMax(N->val[1],1);
     v.x = ROUND(Po->val[0] + lamb * N->val[0]);
     v.y = ROUND(Po->val[1] + lamb * N->val[1]);
-
-    if (*p1 != v && ValidVoxel(img, v))
+    if (isValidPoint(img, v)){
+        found=1;
         *pn = v;
+    }
 
     // third
     // lamb= -x0/Nx
-    lamb = -(Po->val[1])/N->val[1];
+    lamb = -(Po->val[1])/iftMax(N->val[0],1);
     v.x = ROUND(Po->val[0] + lamb * N->val[0]);
     v.y = ROUND(Po->val[1] + lamb * N->val[1]);
-    if (*p1 != v && ValidVoxel(img, v))
+    if (isValidPoint(img, v)){
+        found=1;
         *pn = v;
+
+    }
 
     // forth
     // lamb= (nx-1 - x0)/Nx
-    lamb = (nx - Po->val[0])/N->val[0];
+    lamb = (nx - Po->val[0])/iftMax(N->val[0],1);
+
     v.x = ROUND(Po->val[0] + lamb * N->val[0]);
     v.y = ROUND(Po->val[1] + lamb * N->val[1]);
     //*pn = GetVoxelIndex(img, v);
-    if (*p1 != v && ValidVoxel(img, v))
+    if (isValidPoint(img, v)){
+        found=1;
         *pn = v;
+    }
 
+    
 
-    //return 0;
+    // encontrar o mais proximo (menor lambda) e e mais longe (maior lambda)
+    // maior lambda sera o pn e o menor o p1
+
+    return found;
 }
 
 
@@ -153,15 +216,16 @@ iftMatrix *imagePixelToMatrix(iftImage *img, int p)
     return pixMat;
 }
 
-
 /* this function applies the fast Radon transform (i.e. it uses the DDA algorithm) */
 iftImage *fastRadonTransform(iftImage *img)
 {
     float D = sqrt(img->xsize*img->xsize + img->ysize*img->ysize);
     
     iftMatrix* normal,*normalVec;
-    int Nx, Ny;
+    int Nx, Ny,i;
+    float intensity;
     iftVoxel p1, pn;
+    float* lineVals;
 
     Nx=Ny=D;
     iftImage *R = iftCreateImage(Nx, Ny, 1);
@@ -169,41 +233,67 @@ iftImage *fastRadonTransform(iftImage *img)
     for(int theta = 0; theta < 180; theta++) {
 
         iftMatrix *M = createRadonMatrix(img, theta);
-        iftImage *imgQ = iftCreateImage(D, D, 1);
+        //iftImage *imgQ = iftCreateImage(D, D, 1);
 
 
         // normal calculation
         normalVec =  iftCreateMatrix(1, 4);
-        normalVec->val[1] = normalVec->val[2] = 1.0;
-        normalVec->val[0]=0;
+        normalVec->val[1] = 1.0;
+        normalVec->val[0] = 0.0;
+        normalVec->val[2] = 1.0;
+        normalVec->val[3] = 0.0;
 
         // percorrendo a linha
         for(int p = 0; p < R->n; p++) {
-
+            //printf("p = %d\n", p);
+            
             // Po = M-1 * p
-            iftMatrix *I_ = imagePixelToMatrix(img, p);
+            //iftMatrix *I_ = imagePixelToMatrix(img, p);
+            iftMatrix *I_ = iftCreateMatrix(1, 4);
+            iftMatrixElem(I_, 0, 0) = p;
+            iftMatrixElem(I_, 0, 1) = -D/2;
+            iftMatrixElem(I_, 0, 2) = 0;
+            iftMatrixElem(I_, 0, 3) = 1;
+
+
+            // rotating line
             iftMatrix *P0_line = iftMultMatrices(M, I_);
+            // iftPrintMatrix(M);
 
-
-            // normal = Rz * normal
-            iftMatrix *rotMatrix = iftRotationMatrix(IFT_AXIS_Z, theta);
-            normal = iftMultMatrices(rotMatrix, normalVec);
+            // normal = M * normal
+            //iftMatrix *rotMatrix = iftRotationMatrix(IFT_AXIS_Z, theta);
+            normal = iftMultMatrices(M, normalVec);
             
+            if(findIntersection(P0_line, img, normal,img->xsize,img->ysize, &p1, &pn))
+            {
+                //printf("%d,%d\n",pn.x, pn.y);
+                // chamar o DDA
+                // atribuir o valor obtido de J em Pi
+                
+                lineVals = DDA(img, p1, pn);
 
-            Intersection(P0_line, img, normal,img->xsize,img->ysize, &p1, &pn)
-            // chamar o DDA
-            // atribuir o valor obtido de J em Pi
+                intensity = lineVals[0];
+                
+                for (i = 1; i < 200; i++)
+                    intensity+= lineVals[i];
+                
+                R->val[p] = intensity;
 
-        
-            
-               
+            }
+            else{
+                // sem intercessao
+                
+                R->val[p] = 0;
+            }
         }
+        
     }
 
     return R;
 }
 
-int main(int argc, char const *argv[])
+
+int main(int argc, char *argv[])
 {
     if (argc != 2)
         iftError("Usage: Reconstruction <input-image.png>","main");
@@ -215,18 +305,15 @@ int main(int argc, char const *argv[])
     iftImage *img = iftReadImageByExt(imgFileName);
     iftImage *imgRadon = fastRadonTransform(img);
     printf("Time to compute the Radon Transform: %s\n", iftFormattedTime(iftCompTime(t1, iftToc())));
+    iftImage *normalizedImage= iftNormalize(imgRadon,0,255);
 
     /* save the resulting images */
-    //char fileName[256];
-    //sprintf(fileName, "fast_radon_transform_%s.png", iftFilename(imgFileName, iftFileExt(imgFileName)));
-    //iftWriteImageByExt(imgRadon, fileName);
+    char fileName[256];
+    sprintf(fileName, "fast_radon_transform_%s.png", iftFilename(imgFileName, iftFileExt(imgFileName)));
+    iftWriteImageByExt(normalizedImage, fileName);
 
     iftDestroyImage(&img);
     iftDestroyImage(&imgRadon);
 
-    return 0;
+    return(0);
 }
-
-
-
-
